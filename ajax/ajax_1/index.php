0f58b87f2dd75dac73d77837ca2d1e565536a8eb
jonas18121 <jonasxtrm971@gmail.com>
2020-11-04 17:05:27 +0100

    exo

--- ajax/ajax_1/index.php
+++ ajax/ajax_1/index.php
@@ -9,6 +9,7 @@ function pairImpair($nombre)
 {
     return ($nombre&1) ? "{$nombre} est impair " : "{$nombre} est pair ";
 }
+//var_dump('ok');die;
 
 if ($_POST) {
     
@@ -16,10 +17,12 @@ if ($_POST) {
     /* if (isset($_POST['nombre']) && ctype_digit($_POST['nombre'])) {
         $nombre  = $_POST['nombre'];
         $reponse = pairImpair($nombre);
+        echo json_encode($reponse);
     }else {
         $reponse = "information incorecte";
-    }
-    echo json_encode($reponse); */
+        echo json_encode($reponse);
+    } */
+    
     
     # recherche fruit
     if (isset($_POST['recherche_fruit'])){
@@ -42,5 +45,49 @@ if ($_POST) {
 
         echo json_encode($fruits);
     }
+
+    # recherche ville
+    if (isset($_POST['recherche_ville']) && !empty($_POST['recherche_ville'])) {
+        // var_dump($_POST);die;
+
+        $nom_ville = $_POST['recherche_ville'];
+        $villes = [];
+
+        $sql = "SELECT nom, postal FROM ville WHERE nom LIKE ? ORDER BY population DESC ";
+
+        $ville = $bdd->prepare($sql);
+        $ville->bind_param("s", $r_nom);// prend en compte les ? dans la requete
+        $r_nom = "%" . $nom_ville . "%";
+        $ville->execute();
+        $ville->bind_result($r_nom, $r_postal);// prend en compte les éléments ecrit entre SELECT et FROM
+
+        while ($ville->fetch()) {
+        
+            $villes[] = ["nom" => $r_nom, "postal" => $r_postal]  ;
+        }
+
+        echo json_encode($villes);
+    }
+    elseif (isset($_POST['recherche_code_postal'])  && !empty($_POST['recherche_code_postal'])) {
+        // var_dump($_GET);die;
+
+        $code_postal = $_POST['recherche_code_postal'];
+        $villes = [];
+
+        $sql = "SELECT nom, postal FROM ville WHERE postal LIKE ? ORDER BY population DESC ";
+
+        $ville = $bdd->prepare($sql);
+        $ville->bind_param("s", $r_postal);// prend en compte les ? dans la requete
+        $r_postal = "%" . $code_postal . "%";
+        $ville->execute();
+        $ville->bind_result($r_nom, $r_postal);// prend en compte les éléments ecrit entre SELECT et FROM
+
+        while ($ville->fetch()) {
+        
+            $villes[] = ["nom" => $r_nom, "postal" => $r_postal]  ;
+        }
+
+        echo json_encode($villes);
+    } 
 }
 
